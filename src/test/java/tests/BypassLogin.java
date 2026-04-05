884c68cd015a3d4249b3fe8de56a07ca49fbd23e
Seenuvasan saravanan <seenuvasan.s@prodapt.com>
2026-04-05 15:03:57 +0530

    removed cookies

--- src/test/java/tests/BypassLogin.java
+++ src/test/java/tests/BypassLogin.java
@@ -9,11 +9,6 @@ public class BypassLogin extends BaseTest {
     @Test
     public void byPassLoginSF(){
         getDriver().get(ConfigReader.getProperty("https://cwc--uatsfdc.sandbox.lightning.force.com/"));
-        // Add cookie
-        Cookie cookie = new Cookie("sid", "00D7d000008qWKM!AQEAQITaLG8uVywFlhKw6Pve8avca5fmpGtgErYQkf7vfOWceVKOkki_GVLwxBxrBQnJjXGaRkzbx53r3D1rsh9bGZ1LGHiI");
-        getDriver().manage().addCookie(cookie);
 
-// Refresh to apply session
-        getDriver().navigate().refresh();
     }
 }
